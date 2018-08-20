b238c66a592eb8908dc2ebcf16cf8d2086cb1b0f
LeeJungHyun <leeu4728@gmail.com>
2018-08-20 16:06:13 +0900

    2439

---
+++ 2439.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int in;
+    cin >> in;
+
+    for(int i = in; i >= 1 ; i--){ 
+        for(int j = 1; j <= in ; j++){
+            if(j < i){
+                cout << " ";
+            }
+            else{
+                cout << "*";
+            }
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
