e99be1b7842ec6e2c5d91fea5433c53a508e66c6
LeeJungHyun <leeu4728@gmail.com>
2018-08-20 15:48:45 +0900

    2741

---
+++ 2741.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int in;
+    cin >> in;
+
+    for(int i = 1; i <= in ; i++){ 
+        cout << i << "\n";
+    }
+
+    return 0;
+}
