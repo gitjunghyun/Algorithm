409dd6114e97ade4043d969c7b50e70daa88d041
LeeJungHyun <leeu4728@gmail.com>
2018-08-25 14:15:05 +0900

    1065

---
+++ 1065.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int num(int num);
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int cnt = 0;
+    for(int i = 1 ; i <= n ; i++){
+        if(num(i) == 1){
+            cnt++;
+        }
+    }
+    cout << cnt;
+
+    return 0;
+}
+
+int num(int num) {
+    int h,t,o;
+    if(num < 100){
+        return 1;
+    }else if(num = 1000){
+        return 0;
+    }else{
+        h = num/100;
+        t = (num%100)/10;
+        o = num%10;
+        if((h-t) == (t-o)){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+}
