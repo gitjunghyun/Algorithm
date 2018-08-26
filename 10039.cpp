d92310170e37073aef451b0e2a9647d5420ed58b
LeeJungHyun <leeu4728@gmail.com>
2018-08-26 18:26:17 +0900

    10039

---
+++ 10039.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int arr[5];
+    for(int i = 0; i < 5 ; i++){
+        cin >> arr[i];
+    }
+
+    int sum = 0;
+    for(int i = 0; i < 5; i++) {
+        if(arr[i] < 40){
+            arr[i] = 40;
+        }
+        sum = sum + arr[i];
+    }
+    cout << sum/5;
+
+    return 0;
+}
